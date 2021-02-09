b242870295da5f40bad551edbc5babc5881d9b9b
sistestuser2020 <68368050+sistestuser2020@users.noreply.github.com>
2021-02-09 14:57:46 +0900

    Update getCount.php

--- Introduction/php/getCount.php
+++ Introduction/php/getCount.php
@@ -1,5 +1,4 @@
 <?php
-// *メイン ****************************************
 if (!empty($_GET['countid'])) {
     $id   = empty($_GET['countid']) ? "" : $_GET['countid'];
     $res = getVoteCount($id);
@@ -8,10 +7,7 @@ if (!empty($_GET['countid'])) {
     echo json_encode("syntax error");
  }
 
-// *関数 ******************************************
-
 function getVoteCount($id){
- //print "***********************";
  //カウント数を書きだしてあるファイル名
  $fileName = "log/" . $id . ".count";
 
@@ -27,6 +23,4 @@ function getVoteCount($id){
 
 return $vote;
 }
-
-
 ?>
