b0a042227e5046c17024f63ce98c15b0143e7e7a
sistestuser2020 <68368050+sistestuser2020@users.noreply.github.com>
2021-03-11 17:14:36 +0900

    Update script.js

--- Introduction/javascript/script.js
+++ Introduction/javascript/script.js
@@ -74,24 +74,31 @@ $(function() {
 
 //omikuji_modal
 $(function() {
-	$('.js-modal-open').on('click',function(){
-		var position = $(this).offset().top; //ヘッダの高さ分位置をずらす
-		var img = new Array (
+	$('.js-modal-open').on('click',function(){	//要素=js-modal-open（クリックしてみて）をクリックされると以下ロジックを実行
+		var img = new Array (			//出力するイメージファイルを配列化
 			'images/omikuji_daikichi.png',
 			'images/omikuji_chuukichi.png',
 			'images/omikuji_syoukichi.png',
 			'images/omikuji_kyou.png',
 		);
-		var random = Math.floor(Math.random() * img.length);
-		$('.omikuji-modal-content').append('<p>あなたの今日の運勢は</p>');
-		$('.omikuji-modal-content').append('<P><img src =' +img[random]+ '></p>');
+		var random = Math.floor(Math.random() * img.length);	//0～配列数までの間でランダムに数字を生成し小数点以下切り捨てて整数化
+		$('.omikuji-modal-content').append('<p>あなたの今日の運勢は</p>');	//モーダルコンテンツへHTMLソースを埋め込み
+		$('.omikuji-modal-content').append('<P><img src =' +img[random]+ '></p>');	//ランダム整数に該当する位置のイメージをセット
 		$('.omikuji-modal-content').append('<a class="js-modal-close" href="">閉じる</a>');
-		$('.js-modal').fadeIn();
-		return false;
+		var h1 = $('#header_inner').height();	//ヘッダーID=header_innerの高さを取得
+		var h2 = $('.CoffeeBrake_Top').height();	//ヘッダー要素=CoffeeBrake_Topの高さを取得
+		var h3 = h1 + h2
+		$('.js-modal,.omikuji-modal-bg').css('margin-top', - h3);	//モーダルやモーダル背景のTOP位置がヘッダー直下になってしまう対策としてヘッダー分の高さ分上へ移動
+		var scrollPosition = $(window).scrollTop();	//現在のスクロール座標位置を取得
+		$('body').addClass('fixed').css({'top': -scrollPosition});	//モーダル表示中はスクロールできないようにbodyの座標位置を固定化
+		$('.js-modal').fadeIn();	//モーダルをフェードイン表示
+		return false;	//元の画面は更新しない
 	});
-	$('.js-modal-close').on('click',function(){
-		$('.omikuji-modal-content').empty();
-		$('.js-modal').fadeOut();
+	$('.js-modal-close').on('click',function(){	//要素=js-modal-close（閉じるとかモーダル背景）をクリックされると以下ロジックを実行
+		$('.js-modal').fadeOut();	//モーダルをフェードアウト消去
+		$('.omikuji-modal-content').empty();	//モーダルコンテンツに先ほどのHTMLソースが残存しているので消去
+		$('body').removeClass('fixed').css({'top': 0});		//bodyを固定化していた定義を開放
+		window.scrollTo( 0 , scrollPosition );			//スクロール位置が初期化されているので元の座標位置へ移動
 		return false;
 	});
 });
