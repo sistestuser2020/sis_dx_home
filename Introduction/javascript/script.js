c73393ab4150fb5f63b9115ab97fc455f2aff9b0
sistestuser2020 <68368050+sistestuser2020@users.noreply.github.com>
2021-03-30 10:39:51 +0900

    Update script.js

--- Introduction/javascript/script.js
+++ Introduction/javascript/script.js
@@ -126,3 +126,14 @@ $(function() {
 		return false;
 	});
 });
+
+//ramen_modal
+$(".textmodal").modaal({
+	overlay_close:false,//モーダル背景クリック時に閉じるか
+	before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+});
